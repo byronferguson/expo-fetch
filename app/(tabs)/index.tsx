f3d4b63c9db6c9b4c443f8f1464611b4519b5ad1
Byron Ferguson <ferguson.bs@gmail.com>
2025-04-04 10:05:07 -0500

    failing example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,98 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
 
-export default function HomeScreen() {
+function HomeScreen() {
+  // const [username, setUsername] = useState('fakefamily@ail.com');
+  // const [password, setPassword] = useState('password');
+  const [username, setUsername] = useState('john_doe');
+  const [password, setPassword] = useState('pass123');
+  const [token, setToken] = useState('');
+
+  async function attemptLogin(username: string, password: string) {
+    try {
+      const res = await fetch('https://fakestoreapi.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!res.ok || res.status !== 201) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await res.json();
+      setToken(data.token);
+      return data;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  async function handleLogin() {
+    // Call the login service
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    await attemptLogin(username, password);
+  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <View style={{ gap: 12 }}>
+        <Text style={styles.headline}>SignIn</Text>
+
+        <Text>Username:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your username..."
+          value={username}
+          onChangeText={text => setUsername(text)}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+
+        <Text>Password:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={{ backgroundColor: '#205caa', padding: 10, borderRadius: 10 }}
+        >
+          <Text style={{ color: 'white', textAlign: 'center', fontWeight: 600 }}>LOGIN</Text>
+        </TouchableOpacity>
+        <Text>Token: {token ?? ''}</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: 'white',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  headline: {
+    textAlign: 'center',
+    marginTop: -100,
+    marginBottom: 50,
+    fontWeight: 700,
+    fontStyle: 'italic',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: 'grey',
   },
 });
+
+export default HomeScreen;
